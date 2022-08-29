0d5d27f4fcadda3d4861ddff9ae35d0e11d4c650
siri100 <71384872+siri100@users.noreply.github.com>
2022-08-29 20:22:00 +0530

    c
    c

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
 const navbar = document.querySelector('.navbar');
+const abtBtn = document.querySelector('.about-btn');
+const abtSection = document.querySelector('.about');
+const achvBtn = document.querySelector('.achv');
+const achvSection = document.querySelector('.services');
+const skillsBtn = document.querySelector('.skills-btn');
+const skillsSection = document.querySelector('.skills');
 
-window.addEventListener('scroll' , function(){
+window.addEventListener('scroll', function () {
 
-    if(window.scrollY>20){
+    if (window.scrollY > 20) {
         navbar.classList.add('sticky');
         console.log(window.screenY);
-      }else{
-          navbar.classList.remove('sticky');
-      }
+    } else {
+        navbar.classList.remove('sticky');
+    }
 })
 
 //Typing animation
 
-let typed = new Typed(".typing" ,{
-    strings : ["QA Automation Engineer..." , "Freelancer..." , "Web Developer..."],
-    typeSpeed : 100,
-    backSpeed : 60,
-    loop : true
-  })
+let typed = new Typed(".typing", {
+    strings: ["QA Automation Engineer...", "Freelancer...", "Web Developer..."],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
 
 
+// Button scrolling
+abtBtn.addEventListener('click', function (e) {
+
+    abtSection.scrollIntoView({ behavior: 'smooth' });
+});
+achvBtn.addEventListener('click', function (e) {
+
+    achvSection.scrollIntoView({ behavior: 'smooth' });
+});
+skillsBtn.addEventListener('click', function (e) {
+
+    skillsSection.scrollIntoView({ behavior: 'smooth' });
+});
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     // $(window).scroll(function(){
     //     if(this.scrollY > 20){
     //         $('.navbar').addClass("sticky");
@@ -31,9 +50,15 @@ $(document).ready(function(){
     //     }
     // })
     //toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active")
         $('.menu-btn i').toggleClass("active")
     })
 });
+
+
+
+
+
+
 
